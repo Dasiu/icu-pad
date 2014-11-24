bdc05bce611fb7ab8127b890393caba15035757e
Dasiu <adam.butkiewicz@gmail.com>
2014-11-24 21:17:38 +0100

    Cleaned project up
    * removed redundant configuration and comments
    * fixed O/R mapping
    * added example configuration file with DB connection string and JPA/Hibernate properties

--- src/main/java/com/icupad/domain/AuditableEntity.java
+++ src/main/java/com/icupad/domain/AuditableEntity.java
@@ -4,12 +4,14 @@ import com.icupad.domain.user.User;
 import org.springframework.data.annotation.LastModifiedBy;
 import org.springframework.data.annotation.LastModifiedDate;
 
+import javax.persistence.ManyToOne;
 import javax.persistence.MappedSuperclass;
 import java.time.LocalDateTime;
 
 @MappedSuperclass
 public abstract class AuditableEntity extends BaseEntity {
     @LastModifiedBy
+    @ManyToOne
     private User user;
     @LastModifiedDate
     private LocalDateTime modificationDate;
--- src/main/java/com/icupad/domain/BaseEntity.java
+++ src/main/java/com/icupad/domain/BaseEntity.java
@@ -1,10 +1,12 @@
 package com.icupad.domain;
 
+import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
 import javax.persistence.MappedSuperclass;
 
 @MappedSuperclass
 public class BaseEntity {
     @Id
+    @GeneratedValue
     private Long id;
 }
