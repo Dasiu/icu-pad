ea96e63b615f722c10eeca9abade1bd8f33533e6
Dasiu <adam.butkiewicz@gmail.com>
2015-01-17 21:02:19 +0100

    #13 Added update patient details handler

--- src/test/java/com/icupad/service/hl7_server/PatientDetailsUpdateTest.java
+++ src/test/java/com/icupad/service/hl7_server/PatientDetailsUpdateTest.java
@@ -1,15 +1,18 @@
 package com.icupad.service.hl7_server;
 
+import ca.uhn.hl7v2.AcknowledgmentCode;
 import ca.uhn.hl7v2.DefaultHapiContext;
 import ca.uhn.hl7v2.HL7Exception;
 import ca.uhn.hl7v2.HapiContext;
 import ca.uhn.hl7v2.app.Connection;
 import ca.uhn.hl7v2.app.Initiator;
 import ca.uhn.hl7v2.llp.LLPException;
-import ca.uhn.hl7v2.model.Message;
+import ca.uhn.hl7v2.llp.MinLowerLayerProtocol;
 import ca.uhn.hl7v2.model.v23.message.ACK;
-import ca.uhn.hl7v2.model.v23.message.ADT_A01;
+import ca.uhn.hl7v2.model.v23.message.ADT_A08;
 import com.icupad.Application;
+import com.icupad.domain.Patient;
+import com.icupad.domain.Sex;
 import com.icupad.service.PatientService;
 import com.icupad.service.StayService;
 import org.junit.After;
@@ -24,14 +27,16 @@ import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 import java.io.IOException;
 
-import static com.icupad.test_data.HL7Messages.patientRegistrationMessage;
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.fail;
+import static com.icupad.service.hl7_server.HL7TestUtils.getAcknowledgmentCode;
+import static com.icupad.test_data.HL7Messages.patientDetailsUpdateMessage;
+import static com.icupad.test_data.Patients.createAdamKowalski;
+import static junit.framework.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
 
 @ActiveProfiles("test")
 @RunWith(SpringJUnit4ClassRunner.class)
 @SpringApplicationConfiguration(classes = {Application.class})
-public class HL7ServerTest {
+public class PatientDetailsUpdateTest {
     private static final String host = "localhost";
 
     @Value("${hl7_server.port}")
@@ -47,14 +52,16 @@ public class HL7ServerTest {
     private Initiator initiator;
 
     @Autowired
-    private StayService stayService;
+    private PatientService patientService;
 
     @Autowired
-    private PatientService patientService;
+    private StayService stayService;
 
     @Before
     public void before() throws HL7Exception {
         context = new DefaultHapiContext();
+        context.setLowerLayerProtocol(new MinLowerLayerProtocol(true));
+
         connection = context.newClient(host, port, useSSL);
         initiator = connection.getInitiator();
     }
@@ -68,34 +75,25 @@ public class HL7ServerTest {
     }
 
     @Test
-    public void shouldResponseValidMSASegment() throws HL7Exception, IOException, LLPException {
-        Message messageWithMSHSegment = context.getGenericParser().parse(patientRegistrationMessage);
+    public void shouldUpdatePatientDetails() throws HL7Exception, IOException, LLPException {
+        Patient adamKowalski = createAdamKowalski();
+        patientService.save(adamKowalski);
+        ADT_A08 adt_a08 = (ADT_A08) context.getGenericParser().parse(patientDetailsUpdateMessage);
 
-        ACK response = (ACK) initiator.sendAndReceive(messageWithMSHSegment);
+        ACK ack = (ACK) initiator.sendAndReceive(adt_a08);
 
-        assertMessageControlId(response, messageWithMSHSegment);
-        assertAcknowledgmentCode(response);
+        Patient patient = patientService.findByHl7Id(adamKowalski.getHl7Id());
+        assertNotNull(patient);
+        assertEquals(Sex.MALE, patient.getSex());
+        assertEquals(AcknowledgmentCode.CA, getAcknowledgmentCode(ack));
     }
 
     @Test
-    public void shouldResponseErrorACKWhenInvalidPatientRegistrationMessageReceived() {
-        fail("not implemented yet");
-    }
+    public void shouldResponseErrorACKIfPatientDoesNotExist() throws HL7Exception, IOException, LLPException {
+        ADT_A08 adt_a08 = (ADT_A08) context.getGenericParser().parse(patientDetailsUpdateMessage);
 
-    @Test
-    public void shouldResponseErrorACKWhenAlreadyCorrectlyHandledMessageHasBeenReceivedAgain() {
-        fail("not implemented yet");
-    }
+        ACK ack = (ACK) initiator.sendAndReceive(adt_a08);
 
-    private void assertAcknowledgmentCode(ACK response) {
-        String actualAcknowledgementCode = response.getMSA().getAcknowledgementCode().getValue();
-        assertEquals("CA", actualAcknowledgementCode);
-    }
-
-    private void assertMessageControlId(ACK response, Message messageWithMSHSegment) {
-        ADT_A01 adt = (ADT_A01) messageWithMSHSegment;
-        String expectedMessageId = adt.getMSH().getMessageControlID().getValue();
-        String actualMessageId = response.getMSA().getMessageControlID().getValue();
-        assertEquals(expectedMessageId, actualMessageId);
+        assertEquals(AcknowledgmentCode.CE, getAcknowledgmentCode(ack));
     }
 }
