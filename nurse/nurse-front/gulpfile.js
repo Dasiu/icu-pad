2cb4787830f44d0a5fb3c4adb9047208dca13d54
Maciej <m_cz@op.pl>
2015-03-19 13:27:04 +0100

    fix some bugs

--- nurse/nurse-front/gulpfile.js
+++ nurse/nurse-front/gulpfile.js
@@ -207,7 +207,7 @@ gulp.task('js', function() {
     streamqueue({ objectMode: true },
       gulp.src(config.vendor.js),
       gulp.src('./src/js/**/*.js').pipe(ngFilesort()),
-      gulp.src(['src/templates/**/*.html']).pipe(templateCache({ module: 'ICUPadNurse' }))
+      gulp.src(['src/templates/**/*.html']).pipe(templateCache({ module: 'ICUPad' }))
     )
     .pipe(sourcemaps.init())
     .pipe(concat('app.js'))
