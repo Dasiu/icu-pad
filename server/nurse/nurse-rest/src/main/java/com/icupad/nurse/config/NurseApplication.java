0c623fb60054773ad0e31210176f70879777f2fc
Maciej <m_cz@op.pl>
2015-04-14 16:25:58 +0200

    fix NurseModuleTests

--- server/nurse/nurse-rest/src/main/java/com/icupad/nurse/config/NurseApplication.java
+++ server/nurse/nurse-rest/src/main/java/com/icupad/nurse/config/NurseApplication.java
@@ -4,13 +4,14 @@ import com.icupad.nurse.repository.NurseFunctionRepository;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.security.SecurityAutoConfiguration;
 import org.springframework.boot.orm.jpa.EntityScan;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @Configuration
-@EnableAutoConfiguration
+@EnableAutoConfiguration(exclude = { SecurityAutoConfiguration.class })
 @EnableJpaRepositories(basePackageClasses = { NurseFunctionRepository.class })
 @ComponentScan(basePackages = { "com.icupad.nurse.*" })
 @EntityScan(basePackages = "com.icupad.nurse.model")
--- server/nurse/nurse-rest/src/test/java/com/icupad/nurse/controller/NurseModuleTest.java
+++ server/nurse/nurse-rest/src/test/java/com/icupad/nurse/controller/NurseModuleTest.java
@@ -1,48 +1,62 @@
 package com.icupad.nurse.controller;
 
+import static com.jayway.restassured.RestAssured.when;
+import static org.assertj.core.api.Assertions.assertThat;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.icupad.nurse.config.NurseApplication;
+import com.icupad.nurse.model.NurseFunction;
 import com.jayway.restassured.RestAssured;
+
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.IntegrationTest;
 import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.test.context.web.WebAppConfiguration;
 
+import java.util.Collection;
+
+@ActiveProfiles("test")
 @RunWith(SpringJUnit4ClassRunner.class)
 @SpringApplicationConfiguration(classes = NurseApplication.class)
 @WebAppConfiguration
 @IntegrationTest("server.port:12345")
 public class NurseModuleTest {
 	
+	@Autowired
+    private ObjectMapper objectMapper;
+	
 	@Value("${server.port}")
     private int port;
 	
 	@Test
 	public void shouldReturnFunctions() {
-//		@SuppressWarnings("unchecked")
-//		Collection<NurseFunction> functions =
-//		when().
-//			get("/nurse/functions").
-//			as(Collection.class);
-//		assertThat(functions).isNotNull();
+		@SuppressWarnings("unchecked")
+		Collection<NurseFunction> functions =
+		when().
+			get("/nurse/functions").
+			as(Collection.class);
+		assertThat(functions).isNotNull();
 	}
 	
 	@Test
 	public void functionsNumberShouldBe10() {
-//		@SuppressWarnings("unchecked")
-//		Collection<NurseFunction> functions =
-//		when().
-//			get("/nurse/functions").
-//			as(Collection.class);
-//		assertThat(functions.size()).isEqualTo(10);
+		@SuppressWarnings("unchecked")
+		Collection<NurseFunction> functions =
+		when().
+			get("/nurse/functions").
+			as(Collection.class);
+		assertThat(functions.size()).isEqualTo(10);
 	}
 	
 	@Before
     public void restAssuredConfig() {
-        RestAssured.port = port;
+		RestAssured.port = port;
     }
 
 }
