e12a46feeb99298702dc0fb4d8f3223ce7ca61b0
Dasiu <adam.butkiewicz@gmail.com>
2015-05-02 17:22:42 +0200

    Added test results module

--- server/test-results/test-results-rest/src/main/java/com/icupad/test_results/config/Application.java
+++ server/test-results/test-results-rest/src/main/java/com/icupad/test_results/config/Application.java
@@ -1,8 +1,9 @@
-package com.icupad;
+package com.icupad.test_results.config;
 
 import com.icupad.repository.RepositoryFactoryBean;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.orm.jpa.EntityScan;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
@@ -10,9 +11,12 @@ import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @Configuration
 @EnableAutoConfiguration
-@ComponentScan
 @EnableJpaAuditing
-@EnableJpaRepositories(value = {"com.icupad.repository"}, repositoryFactoryBeanClass = RepositoryFactoryBean.class)
+@EnableJpaRepositories(value = {"com.icupad.test_results.repository", "com.icupad.repository"},
+        repositoryFactoryBeanClass = RepositoryFactoryBean.class)
+@ComponentScan(basePackages = {"com.icupad.security", "com.icupad.service", "com.icupad.repository",
+        "com.icupad.test_results"})
+@EntityScan(basePackages = {"com.icupad.security", "com.icupad.domain", "com.icupad.test_results.model"})
 public class Application {
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
--- server/test-results/test-results-rest/src/main/java/com/icupad/test_results/config/Seed.java
+++ server/test-results/test-results-rest/src/main/java/com/icupad/test_results/config/Seed.java
@@ -1,7 +1,6 @@
-package com.icupad.config;
+package com.icupad.test_results.config;
 
-import com.icupad.domain.user.Doctor;
-import com.icupad.domain.user.User;
+import com.icupad.domain.User;
 import com.icupad.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Profile;
@@ -15,13 +14,18 @@ public class Seed {
     @Autowired
     private UserService userService;
 
+    @Autowired
+    public Seed(UserService userService) {
+        this.userService = userService;
+    }
+
     @PostConstruct
     public void seed() {
         createUsers();
     }
 
     private void createUsers() {
-        User user = new Doctor();
+        User user = new User();
         user.setLogin("admin");
         user.setPassword("admin");
         user.setEnabled(true);
