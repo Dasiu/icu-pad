f1606324a097b4888652370ad05376a8cedec7be
Dasiu <adam.butkiewicz@gmail.com>
2015-05-31 18:20:37 +0200

    #13 HL7 - Added mappings to complete blood count module

--- server/hl7-gateway/src/main/java/com/icupad/hl7_gateway/test_type_module/complete_blood_count/config/CompleteBloodCountProductionSeed.java
+++ server/hl7-gateway/src/main/java/com/icupad/hl7_gateway/test_type_module/complete_blood_count/config/CompleteBloodCountProductionSeed.java
@@ -27,9 +27,9 @@ public class CompleteBloodCountProductionSeed {
         this.testMappingService = testMappingService;
 
         testMappings = Arrays.asList(
-                new TestMapping("Hematokryt", "Morfologia (krew żylna) - Hematokryt", "%"),
                 new TestMapping("Erytrocyty", "Morfologia (krew żylna) - Erytrocyty", "10^6/µl"),
                 new TestMapping("Hemoglobina", "Morfologia (krew żylna) - Hemoglobina", "g/dl"),
+                new TestMapping("Hematokryt", "Morfologia (krew żylna) - Hematokryt", "%"),
                 new TestMapping("MCV", "Morfologia (krew żylna) - MCV", "fl"),
                 new TestMapping("MCH", "Morfologia (krew żylna) - MCH", "pg"),
                 new TestMapping("MCHC", "Morfologia (krew żylna) - MCHC", "g/dl"),
@@ -41,7 +41,28 @@ public class CompleteBloodCountProductionSeed {
                 new TestMapping("MPV", "Morfologia (krew żylna) - MPV", "fl"),
                 new TestMapping("P-LCR", "Morfologia (krew żylna) - P-LCR", "%"),
                 new TestMapping("PCT", "Morfologia (krew żylna) - PCT", "%"),
-                new TestMapping("pH", "Gazometria (krew tętnicza) - pH", "brak")
+                new TestMapping("Erytrocyty", "Morfologia (krew włośniczkowa) -  Erytrocyty", "10^6/µl"),
+                new TestMapping("Hemoglobina", "Morfologia (krew włośniczkowa) -  Hemoglobina", "g/dl"),
+                new TestMapping("Hematokryt", "Morfologia (krew włośniczkowa) -  Hematokryt", "%"),
+                new TestMapping("MCV", "Morfologia (krew włośniczkowa) -  M C V", "fl"),
+                new TestMapping("MCH", "Morfologia (krew włośniczkowa) -  M C H", "pg"),
+                new TestMapping("MCHC", "Morfologia (krew włośniczkowa) -  M C H C", "g/dl"),
+                new TestMapping("Płytki", "Morfologia (krew włośniczkowa) -  Płytki", "10^3/ul"),
+                new TestMapping("Leukocyty", "Morfologia (krew włośniczkowa) -  Leukocyty", "10^3/ul"),
+                new TestMapping("RDW-SD", "Morfologia (krew włośniczkowa) -  RDW-SD", "fl"),
+                new TestMapping("RDW-CV", "Morfologia (krew włośniczkowa) -  RDW-CV", "%"),
+                new TestMapping("PDW", "Morfologia (krew włośniczkowa) -  P D W", "fl"),
+                new TestMapping("MPV", "Morfologia (krew włośniczkowa) -  MPV", "fl"),
+                new TestMapping("P-LCR", "Morfologia (krew włośniczkowa) -  P-LCR", "%"),
+                new TestMapping("PCT", "Morfologia (krew włośniczkowa) -  P C T", "%"),
+                new TestMapping("Erytrocyty", "Morfologia (krew tętnicza) - Erytrocyty", "10^6/µl"),
+                new TestMapping("Hemoglobina", "Morfologia (krew tętnicza) - Hemoglobina", "g/dl"),
+                new TestMapping("Hematokryt", "Morfologia (krew tętnicza) - Hematokryt", "%"),
+                new TestMapping("MCV", "Morfologia (krew tętnicza) - MCV", "fl"),
+                new TestMapping("MCH", "Morfologia (krew tętnicza) - MCH", "pg"),
+                new TestMapping("MCHC", "Morfologia (krew tętnicza) - MCHC", "g/dl"),
+                new TestMapping("Płytki", "Morfologia (krew tętnicza) - Płytki", "10^3/ul"),
+                new TestMapping("Leukocyty", "Morfologia (krew tętnicza) - Leukocyty", "10^3/ul")
         );
     }
 
