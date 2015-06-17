b8b1679c2a17075e889774e01dfb9c3fad91d020
Maciej <m_cz@op.pl>
2015-06-17 23:48:25 +0200

    add form service

--- server/nurse/nurse-rest/src/main/java/com/icupad/common/controller/FormController.java
+++ server/nurse/nurse-rest/src/main/java/com/icupad/common/controller/FormController.java
@@ -1,20 +1,75 @@
 package com.icupad.common.controller;
 
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.icupad.common.service.FormService;
+import com.icupad.form.model.FormTemplate;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.stream.Collectors;
+
 @RestController
 @RequestMapping("/form")
 public class FormController {
+    
+    private static final String FORM_ID = "form_domain";
 
-    @RequestMapping(value = "/{module}/{formName}", method = RequestMethod.GET)
-    public ResponseEntity<?> getFormTemplate(@PathVariable("module") String module, @PathVariable("formName") String formName) {
+    private static final String FORM_NAME = "form_name";
+
+    private static final TypeReference<HashMap<String,Object>> MAP_REF = new TypeReference<HashMap<String,Object>>() {};
+    
+    private final FormService formService;
+
+    @Autowired
+    public FormController(FormService formService) {
+        this.formService = formService;
+    }
+
+    @RequestMapping(value = "/model/{module}/{modelName}", method = RequestMethod.GET)
+    public ResponseEntity<?> getFormTemplate(@PathVariable("module") String module, @PathVariable("modelName") String modelName) {
         try {
-            return new ResponseEntity<>(Class.forName("com.icupad." + module + ".dto." + formName).getDeclaredFields(), HttpStatus.OK);
+            return new ResponseEntity<>(Class.forName("com.icupad." + module + ".model." + modelName).getDeclaredFields(), HttpStatus.OK);
         } catch (ClassNotFoundException e) {
             return new ResponseEntity<Void>(HttpStatus.NOT_FOUND);
         }
     }
+    
+    @RequestMapping(value = "/{module}/{formId}", method = RequestMethod.GET)
+    public ResponseEntity<String> getForm(@PathVariable("module") String module, @PathVariable("formId") String formId) {
+        return new ResponseEntity<>(formService.findByModuleAndId(module, formId).getBody(), HttpStatus.OK);
+    }
+    
+    @RequestMapping(value = "/{module}", method = RequestMethod.GET)
+    public ResponseEntity<Map<String, String>> getModuleTemplates(@PathVariable("module") String module) {
+        return new ResponseEntity<>(formService.findByModule(module).stream().collect(Collectors.toMap((FormTemplate t) -> t.getFormId(), t -> t.getFormName())), HttpStatus.OK);
+    }
+    
+    @RequestMapping(value = "/{module}", method = RequestMethod.POST)
+    public ResponseEntity<FormTemplate> addTemplate(@PathVariable("module") String module, @RequestBody String content) throws IOException {
+        FormTemplate template = createTemplate(module, content);
+        return new ResponseEntity<>(formService.saveTemplate(template), HttpStatus.CREATED);
+    }
+
+    private FormTemplate createTemplate(String module, String content) throws IOException {
+        ObjectMapper mapper = new ObjectMapper();
+        Map<String, String> extractedContent = mapper.readValue(content, MAP_REF);
+        if (!extractedContent.containsKey(FORM_NAME) || !extractedContent.containsKey(FORM_ID)) {
+            throw new IllegalArgumentException("Form model is not valid");
+        }
+        FormTemplate template = new FormTemplate();
+        template.setFormId(extractedContent.get(FORM_ID));
+        template.setFormName(extractedContent.get(FORM_NAME));
+        template.setModuleName(module);
+        template.setBody(content);
+        return template;
+    }
+
 
 }
---
+++ server/nurse/nurse-service/src/main/java/com/icupad/common/service/FormService.java
@@ -0,0 +1,15 @@
+package com.icupad.common.service;
+
+import com.icupad.form.model.FormTemplate;
+
+import java.util.Collection;
+
+public interface FormService {
+
+    Collection<FormTemplate> findByModule(String module);
+
+    FormTemplate saveTemplate(FormTemplate template);
+
+    FormTemplate findByModuleAndId(String module, String formId);
+
+}
