869dd69610a14fab07272f9015eab8cd390bf03d
Dasiu <adam.butkiewicz+git@gmail.com>
2015-08-14 22:29:07 +0200

    Added QueryDSLSupport

--- server/commons/commons-service/src/main/java/com/icupad/repository/BaseRepository.java
+++ server/commons/commons-service/src/main/java/com/icupad/repository/BaseRepository.java
@@ -2,13 +2,14 @@ package com.icupad.repository;
 
 import com.icupad.domain.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.querydsl.QueryDslPredicateExecutor;
 import org.springframework.data.repository.NoRepositoryBean;
 
 import java.io.Serializable;
 import java.util.List;
 
 @NoRepositoryBean
-public interface BaseRepository<T, ID extends Serializable> extends JpaRepository<T, ID> {
+public interface BaseRepository<T, ID extends Serializable> extends JpaRepository<T, ID>, QueryDslPredicateExecutor<T> {
     List<T> findByCreatedBy(User user);
 }
 
--- server/commons/commons-service/src/main/java/com/icupad/repository/BaseRepositoryImpl.java
+++ server/commons/commons-service/src/main/java/com/icupad/repository/BaseRepositoryImpl.java
@@ -2,14 +2,14 @@ package com.icupad.repository;
 
 import com.icupad.domain.User;
 import org.springframework.data.jpa.repository.support.JpaEntityInformation;
-import org.springframework.data.jpa.repository.support.SimpleJpaRepository;
+import org.springframework.data.jpa.repository.support.QueryDslJpaRepository;
 
 import javax.persistence.EntityManager;
 import java.io.Serializable;
 import java.util.List;
 
 public class BaseRepositoryImpl<T, ID extends Serializable>
-        extends SimpleJpaRepository<T, ID>
+        extends QueryDslJpaRepository<T, ID>
         implements BaseRepository<T, ID> {
     public BaseRepositoryImpl(JpaEntityInformation<T, ID> entityInformation, EntityManager entityManager) {
         super(entityInformation, entityManager);
