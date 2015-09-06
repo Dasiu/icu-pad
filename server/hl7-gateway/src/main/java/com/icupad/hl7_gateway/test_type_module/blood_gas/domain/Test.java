e31ab7ea9ce4467ed6f2b35f3a4bb8711c11f280
Dasiu <adam.butkiewicz+git@gmail.com>
2015-09-06 22:56:07 +0200

    #13 HL7 - Added unique constrains to test entities

--- server/hl7-gateway/src/main/java/com/icupad/hl7_gateway/test_type_module/blood_gas/domain/Test.java
+++ server/hl7-gateway/src/main/java/com/icupad/hl7_gateway/test_type_module/blood_gas/domain/Test.java
@@ -3,8 +3,13 @@ package com.icupad.hl7_gateway.test_type_module.blood_gas.domain;
 import com.icupad.hl7_gateway.core.domain.BaseEntity;
 
 import javax.persistence.Entity;
+import javax.persistence.Table;
+import javax.persistence.UniqueConstraint;
 
 @Entity(name = "blood_gas_test")
+@Table(name = "blood_gas_test", uniqueConstraints = {
+        @UniqueConstraint(columnNames = {"name", "unit"}),
+})
 public class Test extends BaseEntity {
     private String name;
     private String unit;
--- server/hl7-gateway/src/main/java/com/icupad/hl7_gateway/test_type_module/default_test_type/domain/Test.java
+++ server/hl7-gateway/src/main/java/com/icupad/hl7_gateway/test_type_module/default_test_type/domain/Test.java
@@ -3,8 +3,13 @@ package com.icupad.hl7_gateway.test_type_module.default_test_type.domain;
 import com.icupad.hl7_gateway.core.domain.BaseEntity;
 
 import javax.persistence.Entity;
+import javax.persistence.Table;
+import javax.persistence.UniqueConstraint;
 
 @Entity(name = "default_test")
+@Table(name = "default_test", uniqueConstraints = {
+        @UniqueConstraint(columnNames = {"name", "unit"}),
+})
 public class Test extends BaseEntity {
     private String name;
     private String unit;
