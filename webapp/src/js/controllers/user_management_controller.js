1e25da24444358b520d8e60b6d2d37df91c8b7e3
Maciej <m_cz@op.pl>
2015-09-07 18:29:52 +0200

    add users management page

---
+++ webapp/src/js/controllers/user_management_controller.js
@@ -0,0 +1,6 @@
+angular.module('ICUPad.controllers.UserManagement', [])
+
+    .controller('UserManagementController', function($scope, $http, configuration){
+        $scope.title = "users";
+
+    });
