c87b9135de0d0bd345a62ec89e7270a376a37432
Maciej <m_cz@op.pl>
2015-09-26 10:03:55 +0200

    add main method to form application

--- server/form-builder/src/main/java/com/icupad/form/FormBuilderApplication.java
+++ server/form-builder/src/main/java/com/icupad/form/FormBuilderApplication.java
@@ -1,11 +1,17 @@
 package com.icupad.form;
 
 import com.icupad.form.repository.FormTemplateRepository;
+import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.context.embedded.EmbeddedServletContainerFactory;
+import org.springframework.boot.context.embedded.tomcat.TomcatEmbeddedServletContainerFactory;
 import org.springframework.boot.orm.jpa.EntityScan;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+
+import java.util.Collections;
 
 @Configuration
 @EnableAutoConfiguration
@@ -13,4 +19,15 @@ import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 @ComponentScan(basePackages = { "com.icupad.form.*" })
 @EntityScan(basePackages = { "com.icupad.form.model" })
 public class FormBuilderApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FormBuilderApplication.class, args);
+    }
+
+    @Bean
+    public EmbeddedServletContainerFactory servletContainer() {
+        TomcatEmbeddedServletContainerFactory factory = new TomcatEmbeddedServletContainerFactory();
+        factory.setTomcatContextCustomizers(Collections.singletonList(context -> context.setUseHttpOnly(false)));
+        return factory;
+    }
 }
