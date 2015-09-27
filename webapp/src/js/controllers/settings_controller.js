e4b8f3da1141a58de7a091e0267880855f4f8081
Maciej <m_cz@op.pl>
2015-09-27 22:35:30 +0200

    add configuration of form app address

--- webapp/src/js/controllers/settings_controller.js
+++ webapp/src/js/controllers/settings_controller.js
@@ -5,10 +5,19 @@ angular.module('ICUPad.controllers.Settings', [])
         $scope.serverAddress = configuration._serverAddress;
         $scope.serverPort = configuration._serverPort;
 
+        $scope.formServerProtocol = configuration._formServerProtocol ;
+        $scope.formServerAddress = configuration._formServerAddress;
+        $scope.formServerPort = configuration._formServerPort;
+
         $scope.saveConfiguration = function() {
             configuration._serverProtocol = $scope.serverProtocol;
             configuration._serverAddress = $scope.serverAddress;
             configuration._serverPort = $scope.serverPort;
+
+            configuration._formServerProtocol = $scope.formServerProtocol;
+            configuration._formServerAddress = $scope.formServerAddress;
+            configuration._formServerPort = $scope.formServerPort;
+
             $location.path("/login");
         }
     });
--- webapp/src/js/services/configuration-provider.js
+++ webapp/src/js/services/configuration-provider.js
@@ -4,7 +4,15 @@ angular.module('ICUPad.services.Configuration', [])
         this._serverProtocol = "https";
         this._serverAddress = "192.168.0.11";
         this._serverPort = "8443";
+
+        this._formServerProtocol = "http";
+        this._formServerAddress = "192.168.0.11";
+        this._formServerPort = "8070";
+
         this.server = function () {
             return this._serverProtocol + "://" + this._serverAddress + ":" + this._serverPort;
+        };
+        this.formServer = function () {
+            return this._formServerProtocol + "://" + this._formServerAddress + ":" + this._formServerPort;
         };
     }]);
