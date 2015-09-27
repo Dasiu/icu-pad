6124898f1ea63acd64faadf493cc4d7ed871b302
Maciej <m_cz@op.pl>
2015-09-27 22:37:11 +0200

    fix cors problem

---
+++ server/form-builder/src/main/java/com/icupad/form/controller/CorsFilter.java
@@ -0,0 +1,38 @@
+package com.icupad.form.controller;
+
+import org.springframework.core.Ordered;
+import org.springframework.core.annotation.Order;
+import org.springframework.stereotype.Component;
+
+import javax.servlet.*;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@Component
+@Order(Ordered.HIGHEST_PRECEDENCE)
+public class CorsFilter implements Filter {
+    @Override
+    public void init(FilterConfig filterConfig) throws ServletException {
+        // do nothing
+    }
+
+    @Override
+    public void destroy() {
+        // do nothing
+    }
+
+    @Override
+    public void doFilter(ServletRequest req, ServletResponse res, FilterChain chain)
+            throws IOException, ServletException {
+        HttpServletResponse response = (HttpServletResponse) res;
+        HttpServletRequest request = (HttpServletRequest) req;
+        response.setHeader("Access-Control-Allow-Origin", "*");
+        response.setHeader("Access-Control-Allow-Methods", "POST, PUT, GET, OPTIONS, DELETE");
+        response.setHeader("Access-Control-Allow-Headers", "x-requested-with, authorization");
+        response.setHeader("Access-Control-Max-Age", "3600");
+        if (!"OPTIONS".equals(request.getMethod()) ) {
+            chain.doFilter(req, res);
+        }
+    }
+}
